5ffd1bcb1565884c6ba7671661d06ba118f290da
Shubham Dubey <itzshubhamofficial@gmail.com>
2024-07-25 11:43:53 +0530

    API Developed

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, Response, make_response, request
+import requests
+
+app = Flask(__name__)
+
+url = "https://foscos.fssai.gov.in/gateway/commonauth/commonapi/getsearchapplicationdetails/1"
+
+@app.route("/api/v1/get_fssai_details", methods=["POST"])
+def getCADetails():
+    try:
+        licenceNumber = request.json.get("licenceNumber")
+        session = requests.Session()
+
+        session.verify = False
+
+        postBody = {
+            "flrsLicenseNo": licenceNumber
+        }
+
+        response = session.post(url, json=postBody)
+
+        return jsonify(response.json())
+    
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Error in fetching fssai licence Details"})
+
+if __name__ == "__main__":
+    app.run()
